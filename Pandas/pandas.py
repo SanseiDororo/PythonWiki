d3c7147bb8e25499801abeda723bf78d440df6e2
SanseiDororo <philoscopes@gmail.com>
2022-12-10 13:14:37 +0100

    Pandas Statistics Formatted

--- Pandas/pandas.py
+++ Pandas/pandas.py
@@ -75,77 +75,100 @@ def pandas():
                 # DATAFRAME METHODS
 
                 returns basic statistics 
+                
                 ```df.describe()```         
                 
                 returns non missing values for each column or row
+                
                 ```df.count()```
 
                 returns average value. Axis parameter selects row
+                
                 ```df.mean(axis = )```
 
                 returns the total of numerical values
+                
                 ```df.sum()```
 
                 returns the median of values
+                
                 ```df.median()```
 
                 returns the minimum of values
+                
                 ```df.min()```             
                 
                 returns the maximum of values
+                
                 ```df.max()```
 
                 returns the index where the maximum value occurs
+                
                 ```df.idxmax()```           
                 
                 returns the index where the minimum value occurs
+                
                 ```df.idxmin()```
 
                 returns the absolute value of values
+                
                 ```df.abs()```
 
                 returns the standard deviation
+                
                 ```df.std()```
 
                 return the variance
+                
                 ```df.var()```
 
                 return the covariance between two Series or 
                 a covariance matrix for all columns combinations in DataFrame 
+                
                 ```df.cov()```
 
                 returns correlation between two Series 
                 or a correlation matrix for all columns in a DataFrame
+                
                 ```df.corr()```             
                 
                 gets specific quantile
+                
                 ```df.quantile()```        
                 
                 returns cumulative sum
+                
                 ```df.cumsum()```          
                 
                 returns cumulative minimum
+                
                 ```df.cummin()```           
                 
                 returns cumulative maximum
+                
                 ```df.cummax()```           
                 
 
                 #SERIES METHODS
                 
                 gets distinct values od the column
+                
                 ```series.unique()```           
                 
                 returns frequency of values
+                
                 ```series.value_counts()```      
 
                 gets the most common value of the series
+                
                 ```series.mode()```             
                 
                 its acummulative sum
+                
                 ```df.column.cumsum()```        
                 
                 returns corellation between 2 integer values (very useful)
+                
                 ```df.column.corr()```           
 
 
